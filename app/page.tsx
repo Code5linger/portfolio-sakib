7b8d3434133efbf4782d40b52daa446125a0080b
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-07-30 18:44:08 +0600

    Added tailwind styling system
    Added Clsx and Tailwind-merge

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import { cn } from '@/lib/utils';
+
 export default function Home() {
   return (
     <main>
       <h1>
-        <span>Hello</span> World!
+        <span
+          className={cn(
+            'italic text-5xl font-extrabold',
+            ', text-red-600',
+            true && 'underline'
+          )}
+        >
+          Hello
+        </span>{' '}
+        World!
       </h1>
     </main>
   );
